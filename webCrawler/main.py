49c2e9c435645e30c469d39e5bb02317b9ff1eec
Danny Timmer <dtimmer@danteck.nl>
2019-04-18 11:15:33 +0200

    Edited MySQL connection strings for direct connection to our server. Removed the crawled.txt and queue.txt

--- webCrawler/main.py
+++ webCrawler/main.py
@@ -8,12 +8,9 @@ import mysql.connector
 PROJECT_NAME = 'startpagina'
 HOMEPAGE = 'http://startpagina.nl/'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
-QUEUE_FILE = PROJECT_NAME + '/queue.txt'
-CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
-NUMBER_OF_THREADS = 64
+NUMBER_OF_THREADS = 12
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
-
 
 # Create worker threads (will die when main exits)
 def create_workers():
@@ -27,15 +24,28 @@ def create_workers():
 def work():
     while True:
         url = queue.get()
-
         Spider.crawl_page(threading.current_thread().name, url)
         queue.task_done()
 
 
 # Each queued link is a new job
 def create_jobs():
-    print(file_to_set(QUEUE_FILE))
-    for link in file_to_set(QUEUE_FILE):
+    mySQLConnection = mysql.connector.connect(
+        database="project",
+        host="192.168.0.33",
+        user="projectUser",
+        passwd="Anusklep20!",
+        auth_plugin='mysql_native_password'
+    )
+
+    sqlTest = "select link from project.queue"
+    cursor = mySQLConnection.cursor()
+    cursor.execute(sqlTest)
+    wachtrijURL = [item[0] for item in cursor.fetchall()]
+    mySQLConnection.close()
+    cursor.close()
+
+    for link in wachtrijURL:
         queue.put(link)
     queue.join()
     crawl()
@@ -43,7 +53,22 @@ def create_jobs():
 
 # Check if there are items in the queue, if so crawl them
 def crawl():
-    queued_links = file_to_set(QUEUE_FILE)
+    mySQLConnection = mysql.connector.connect(
+        database="project",
+        host="192.168.0.33",
+        user="projectUser",
+        passwd="Anusklep20!",
+        auth_plugin='mysql_native_password'
+    )
+
+    sqlTest = "select link from project.queue"
+    cursor = mySQLConnection.cursor()
+    cursor.execute(sqlTest)
+    wachtrijURL = [item[0] for item in cursor.fetchall()]
+    mySQLConnection.close()
+    cursor.close()
+
+    queued_links = wachtrijURL
     if len(queued_links) > 0:
         print(str(len(queued_links)) + ' links in the queue')
         create_jobs()
--- webCrawler/spider.py
+++ webCrawler/spider.py
@@ -21,8 +21,6 @@ class Spider:
         Spider.project_name = project_name
         Spider.base_url = base_url
         Spider.domain_name = domain_name
-        Spider.queue_file = Spider.project_name + '/queue.txt'
-        Spider.crawled_file = Spider.project_name + '/crawled.txt'
         self.boot()
         self.crawl_page('First spider', Spider.base_url)
 
@@ -31,28 +29,42 @@ class Spider:
     def boot():
         create_project_dir(Spider.project_name)
         create_data_files(Spider.project_name, Spider.base_url)
-        Spider.queue = file_to_set(Spider.queue_file)
-        Spider.crawled = file_to_set(Spider.crawled_file)
+
 
     # Updates user display, fills queue and updates files
     @staticmethod
     def crawl_page(thread_name, page_url):
-        if page_url not in Spider.crawled:
+        # hier controleren of de pagina URL niet al in de cralwed tabel staat van MySQL anders krijgen we dubbelen
+
+        mysqlConnect = mysql.connector.connect(database=Spider.database, host=Spider.host, user=Spider.user,
+                                               passwd=Spider.passwd, auth_plugin='mysql_native_password')
+        sqlTest = "select link from project.crawled WHERE link = '" + page_url + "';"
+        cursor = mysqlConnect.cursor()
+        cursor.execute(sqlTest)
+        alCrawled = cursor.fetchone()
+        cursor.close()
+
+        if alCrawled:
+            print('Pagina is al gecrawled drop! do nothing')
+        else:
             print(thread_name + ' now crawling ' + page_url)
             print('Queue ' + str(len(Spider.queue)) + ' | Crawled  ' + str(len(Spider.crawled)))
             Spider.add_links_to_queue(Spider.gather_links(page_url))
-            Spider.queue.remove(page_url)
-            Spider.crawled.add(page_url)
 
             # Hier vullen we de MySQL crawled tabel met links die verwerkt zijn
-            mysqlConnect = mysql.connector.connect(database=Spider.database, host=Spider.host, user=Spider.user,
-                                                   passwd=Spider.passwd, auth_plugin='mysql_native_password')
-            sql_insert_query = "INSERT INTO crawled(link) VALUES ('"+page_url+"');"
+            sql_insert_query = "INSERT INTO project.crawled(link) VALUES ('"+page_url+"');"
             cursor = mysqlConnect.cursor()
             cursor.execute(sql_insert_query)
             mysqlConnect.commit()
+            cursor.close()
 
-            Spider.update_files()
+        # Hier halen we de queue link uit de MySQL queue tabel
+        Spider.queue.remove(page_url)
+        sql_delete_query = "DELETE FROM project.queue WHERE link = '" + page_url + "';"
+        cursor = mysqlConnect.cursor()
+        cursor.execute(sql_delete_query)
+        mysqlConnect.commit()
+        cursor.close()
 
     # Converts raw response data into readable information and checks for proper html formatting
     @staticmethod
@@ -81,15 +93,11 @@ class Spider:
             Spider.queue.add(url)
 
             # Hier vullen we de MySQL queue tabel met nieuwe linksjes aan de wachtrij
-            mysqlconnect = mysql.connector.connect(database=Spider.database, host=Spider.host, user=Spider.user,
+            mysqlConnect = mysql.connector.connect(database=Spider.database, host=Spider.host, user=Spider.user,
                                                    passwd=Spider.passwd, auth_plugin='mysql_native_password')
             sql_insert_query = "INSERT INTO queue(link) VALUES ('"+url+"');"
-            cursor = mysqlconnect.cursor()
+            cursor = mysqlConnect.cursor()
             cursor.execute(sql_insert_query)
-            mysqlconnect.commit()
-
-
-    @staticmethod
-    def update_files():
-        set_to_file(Spider.queue, Spider.queue_file)
-        set_to_file(Spider.crawled, Spider.crawled_file)
+            mysqlConnect.commit()
+            cursor.close()
+            mysqlConnect.close()
